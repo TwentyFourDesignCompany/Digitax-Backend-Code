39580f823394ec305e085348a0e0afe07174da1d
24 Design Company <77238502+twentyFourDesign@users.noreply.github.com>
2024-09-05 06:30:02 +0100

    configure mongo string

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const dbConnection = async()=>{
-   let connection = await mongoose.connect(`mongodb+srv://talhahaider074:nyPd1ehqbCxRcVu9@cluster0.iv6y2ri.mongodb.net/`)
+   // let connection = await mongoose.connect(`mongodb+srv://talhahaider074:nyPd1ehqbCxRcVu9@cluster0.iv6y2ri.mongodb.net/`)
+   let connection = await mongoose.connect(`mongodb+srv://admin:admin@susdb.bvnawp0.mongodb.net/?retryWrites=true&w=majority&appName=susdb`)
+
+   // const uri = "mongodb+srv://admin:<db_password>@susdb.bvnawp0.mongodb.net/?retryWrites=true&w=majority&appName=susdb";
    if(connection){
     console.log(`connected`)
    }
