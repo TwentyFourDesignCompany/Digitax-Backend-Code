eed94c133090dafb8fd85cd530084e27f2665ce3
24 Design Company <77238502+twentyFourDesign@users.noreply.github.com>
2024-09-05 08:57:13 +0100

    Revert all changes

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const dbConnection = async()=>{
    let connection = await mongoose.connect(`mongodb+srv://talhahaider074:nyPd1ehqbCxRcVu9@cluster0.iv6y2ri.mongodb.net/`)
-
    if(connection){
     console.log(`connected`)
    }
