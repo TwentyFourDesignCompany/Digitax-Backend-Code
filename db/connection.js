48238f09bdacab5e68c8a56e686d93850d25620a
24 Design Company <77238502+twentyFourDesign@users.noreply.github.com>
2024-09-05 13:20:10 +0000

    fix cluster string

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbConnection = async()=>{
-   let connection = await mongoose.connect(`mongodb+srv://contractprojects2022:5QsirHkDPze53aZ@cluster0.iv6y2ri.mongodb.net/?retryWrites=true&w=majority&appName=digitax`)
+   let connection = await mongoose.connect(`mongodb+srv://contractprojects2022:5QsirHkDPze53aZ@digitax.wmku2.mongodb.net/?retryWrites=true&w=majority&appName=digitax`)
    if(connection){
     console.log(`connected`)
    }
