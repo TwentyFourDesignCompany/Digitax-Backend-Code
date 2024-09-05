6d95e25b5da7ef9ded07a1b395807a29ef4cdd91
24 Design Company <77238502+twentyFourDesign@users.noreply.github.com>
2024-09-05 08:42:54 +0100

    Revert conn string

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 
 const dbConnection = async()=>{
-   // let connection = await mongoose.connect(`mongodb+srv://talhahaider074:nyPd1ehqbCxRcVu9@cluster0.iv6y2ri.mongodb.net/`)
-   let connection = await mongoose.connect(`mongodb+srv://admin:xobwE2-cufgib-tofqub@susdb.bvnawp0.mongodb.net/?retryWrites=true&w=majority&appName=susdb`)
+   let connection = await mongoose.connect(`mongodb+srv://talhahaider074:nyPd1ehqbCxRcVu9@cluster0.iv6y2ri.mongodb.net/`)
 
-   // const uri = "mongodb+srv://admin:<db_password>@susdb.bvnawp0.mongodb.net/?retryWrites=true&w=majority&appName=susdb";
    if(connection){
     console.log(`connected`)
    }
